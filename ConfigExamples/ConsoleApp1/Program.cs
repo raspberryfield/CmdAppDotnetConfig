07ae5eac2b75322e17030ec7058ca9f8a9a4b7e9
Fred <admin@raspberryfield.life>
2020-08-04 15:56:53 +0200

    Added test config configurationbuilder json + logging in consoleApp2 with Serilog.Sinks.File

---
+++ ConfigExamples/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World - ConsoleApp1.");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+
+            var config = builder.Build();
+
+            Console.WriteLine(config["TestSetting1"]);
+            Console.WriteLine(config["TestSetting2:SubSetting1"]); //SubSetting1
+
+        }
+    }
+}
---
+++ ConfigExamples/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Serilog;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.File("my_log.txt")
+                    .CreateLogger();
+
+            Log.Information("I am logging now =) .");
+
+        }
+    }
+}
