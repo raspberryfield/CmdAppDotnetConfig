d9617002102a6ccc32f9f2b8877d9564c2c02718
Fred <admin@raspberryfield.life>
2020-08-05 15:41:17 +0200

    Added ConsoleApp3 combine logger and config.

---
+++ ConfigExamples/ConsoleApp3/MessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class MessageHandler
+    {
+        
+        public MessageHandler()
+        {
+
+        }
+
+    }
+}
---
+++ ConfigExamples/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConsoleApp3 - Testing different approaches of logging.");
+            
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();            
+
+
+            Console.WriteLine(config["path_log_directory"]);
+
+            var logger = new LogHandler()._logger;
+
+            logger.Information(">> This is just a test!");
+
+        }
+    }
+}
